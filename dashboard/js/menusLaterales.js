570c95e540cb7ed2c49afbcfdaa9853ee4453bf9
AnnyBarba <aebarbap@gmail.com>
2025-02-26 13:23:24 -0600

    modal para gestión de usuarios con tabla

--- dashboard/js/menusLaterales.js
+++ dashboard/js/menusLaterales.js
@@ -31,9 +31,9 @@ function nuevoUsuario() {
             <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
             <select class="form-select" aria-label="Default select example">
                 <option selected>Tipo de usuario ...</option>
-                <option value="1">Usuario 1</option>
-                <option value="2">Usuario 2</option>
-                <option value="3">Usuario 3</option>
+                <option value="3">Usuario A</option>
+                <option value="2">Usuario B</option>
+                <option value="1">Administrador</option>
             </select>
           </div>
           <div class="input-group mb-3">
@@ -69,58 +69,140 @@ function nuevoUsuario() {
   }
 
 function editarUsuario() {
-
     let titulo = "Editar usuario";
     // Crear el elemento del modal
+    const modal = document.createElement('div');
+    modal.classList.add('modal', 'fade');
+    modal.setAttribute('tabindex', '-1');
+    modal.innerHTML = `
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">${titulo}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
+              <input type="text" class="form-control border-secondary" placeholder="Usuario" aria-label="Usuario" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-regex"></i></span>
+              <input type="password" class="form-control border-secondary" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
+              <input type="text" class="form-control border-secondary" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
+              <select class="form-select" aria-label="Default select example">
+                  <option selected>Tipo de usuario ...</option>
+                  <option value="1">Usuario 1</option>
+                  <option value="2">Usuario 2</option>
+                  <option value="3">Usuario 3</option>
+              </select>
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
+              <select class="form-select" aria-label="Default select example">
+                  <option value="" selected>Estatus de usuario ...</option>
+                  <option value="1">Activo</option>
+                  <option value="0">Inactivo</option>
+              </select>
+            </div>
+
+            </p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-primary">Editar</button>
+          </div>
+        </div>
+      </div>
+    `;
+
+    // Agregar el modal al body del documento
+    document.body.appendChild(modal);
+
+    // Mostrar el modal usando Bootstrap's JavaScript API
+    const bootstrapModal = new bootstrap.Modal(modal);
+    bootstrapModal.show();
+
+    // Eliminar el modal del DOM cuando se cierre
+    modal.addEventListener('hidden.bs.modal', () => {
+      modal.remove();
+    });
+  }
+
+function gestionUsuarios(){
+  let titulo = "Gestión de Usuarios";
+  // Crear el elemento del modal
   const modal = document.createElement('div');
   modal.classList.add('modal', 'fade');
   modal.setAttribute('tabindex', '-1');
   modal.innerHTML = `
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">${titulo}</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="modal-dialog modal-xl">>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">${titulo}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input name="id" id="idHidden" value="" hidden>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+                            <input type="text" class="form-control" placeholder="Buscar..." aria-label="Buscar" aria-describedby="basic-addon1" id="buscar" name="buscar">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1" for="inputGroupSelect01">Perfil</span>
+                            <select class="form-select" id="inputGroupSelect01" value="" selected="selected" name="perfilselect">
+                                <option value="1">Administrador</option>
+                                <option value="2">Usuario A</option>
+                                <option value="3">Usuario B</option>
+                            </select>
+                            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                <input type="radio" class="btn-check" value="1" name="btnradio" id="btnradio1">
+                                <label class="btn btn-outline-success" for="btnradio1"><i class="bi bi-check-lg"></i> Activo</label>
+                            
+                                <input type="radio" class="btn-check" value="2" name="btnradio" id="btnradio2">
+                                <label class="btn btn-outline-danger" for="btnradio2"><i class="bi bi-x-lg"></i> Inactivo</label>
+                            </div>
+                        </div>
+                    </div>
+                
+                <button type="submit" class="btn btn-primary"><i class="bi bi-person-plus"></i> Guardar Cambios</button>
+                <div class="table-responsive mt-3">
+                    <table class="table p-1">
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">Perfil</th>
+                                <th scope="col">Fecha de creación</th>
+                                <th scope="col">Último logIn</th>
+                                <th scope="col">Estatus</th>
+                                <th scope="col">Nombre completo</th>
+                                <th scope="col">Usuario</th>
+                                <th scope="col" class="text-end"><i class="bi bi-people"></i></th>
+                            </tr>
+                        </thead>
+                        <tbody id="tablaUsuariosG">
+                            
+                        </tbody>
+                    </table>
+                </div>
+                <!-- <input type="checkbox" > Mostrar Password  -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i class="bi bi-x-circle-fill"></i> Cancelar</button>
+                <!-- <button type="submit" class="btn btn-primary"><i class="bi bi-person-plus"></i> Guardar Cambios</button> -->
+            </div>
         </div>
-        <div class="modal-body">
-          <p>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
-            <input type="text" class="form-control border-secondary" placeholder="Usuario" aria-label="Usuario" aria-describedby="basic-addon1">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-regex"></i></span>
-            <input type="password" class="form-control border-secondary" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
-            <input type="text" class="form-control border-secondary" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Tipo de usuario ...</option>
-                <option value="1">Usuario 1</option>
-                <option value="2">Usuario 2</option>
-                <option value="3">Usuario 3</option>
-            </select>
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-add"></i></span>
-            <select class="form-select" aria-label="Default select example">
-                <option value="" selected>Estatus de usuario ...</option>
-                <option value="1">Activo</option>
-                <option value="0">Inactivo</option>
-            </select>
-          </div>
-
-          </p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          <button type="button" class="btn btn-primary">Editar</button>
-        </div>
-      </div>
     </div>
   `;
 
@@ -135,4 +217,4 @@ function editarUsuario() {
   modal.addEventListener('hidden.bs.modal', () => {
     modal.remove();
   });
-  }
+}
