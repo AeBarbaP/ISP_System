fc43b5a6c80b10d0f28bf2732432c0505b0d6830
jesusrlv <jesusrlvrojo@gmail.com>
2025-02-26 09:30:41 -0600

    modal registro de pago

---
+++ dashboard/js/registroPago.js
@@ -0,0 +1,24 @@
+function pagoNew() {
+    $("#pago").modal("show");
+
+    // Obtener los elementos input
+    const inputFechaActual = document.getElementById('fechaSolicitud');
+    // Obtener la fecha actual
+    const fechaActual = new Date();
+
+    // Función para formatear la fecha en YYYY-MM-DD
+    function formatearFecha(fecha) {
+        const año = fecha.getFullYear();
+        const mes = String(fecha.getMonth() + 1).padStart(2, '0'); // Meses van de 0 a 11
+        const dia = String(fecha.getDate()).padStart(2, '0');
+        return `${año}-${mes}-${dia}`;
+    }
+
+    // Formatear las fechas
+    const fechaActualFormateada = formatearFecha(fechaActual);
+
+    // Asignar las fechas a los inputs
+    inputFechaActual.value = fechaActualFormateada;
+
+    // Inicia el ajax para los datos del usuario
+}
