655293cbee762e024046c2eac333ab5bb3395f71
unknown <aebarbap@gmail.com>
2025-03-25 20:04:14 -0600

    modales para promociones check

--- dashboard/js/menusLaterales.js
+++ dashboard/js/menusLaterales.js
@@ -821,33 +821,35 @@ function nuevaPromo() {
         </div>
         <div class="modal-body">
           <p>
-          
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-cursor-text"></i></span>
-            <input type="text" class="form-control" placeholder="Nombre del paquete" aria-label="nombre paquete" id="nombre_paquete" aria-describedby="basic-addon1">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-speedometer2"></i></span>
-            <input type="number" class="form-control" placeholder="Velocidad" aria-label="velocidad" id="velocidad_paquete" aria-describedby="basic-addon1">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-cash-coin"></i></span>
-            <input type="text" class="form-control" placeholder="Precio mensual" aria-label="precio" id="precio_paquete" aria-describedby="basic-addon1">
-            <span class="input-group-text" id="basic-addon1">.00</span>
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-toggles"></i></span>
-            <select class="form-select" aria-label="Default select example" id="estatus_paquete">
-                <option value="" selected>Estatus...</option>
-                <option value="1">Activo</option>
-                <option value="0">Inactivo</option>
-            </select>
-          </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-calendar2-week"></i></span>
+              <input type="datetime-local" class="form-control" placeholder="" aria-label="Fecha de inicio" id="fecha_inicio" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-calendar2-week"></i></span>
+              <input type="datetime-local" class="form-control" placeholder="" aria-label="Fecha de fin" id="fecha_fin" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-cursor-text"></i></span>
+              <input type="text" class="form-control" placeholder="Nombre de la promoción" aria-label="nombre promocion" id="nombre_promocion" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-card-checklist"></i></span>
+              <select class="form-select" aria-label="Default select example" id="tipo_promo">
+                  <option value="" selected>Promoción en...</option>
+                  <option value="Mensualidad">Mensualidad</option>
+                  <option value="Instalación">Instalación</option>
+              </select>
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-piggy-bank"></i></span>
+              <input type="number" class="form-control" placeholder="Descuento" aria-label="descuento" id="descuento_promo" aria-describedby="basic-addon1">
+            </div>
           </p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          <button type="button" class="btn btn-primary" onclick="guardarPaquete()">Guardar</button>
+          <button type="button" class="btn btn-primary" onclick="guardarPromocion()">Guardar</button>
         </div>
       </div>
     </div>
@@ -867,7 +869,7 @@ function nuevaPromo() {
 }
 
 
-function editarPaquete() {
+function editarPromo() {
 
   let titulo = "Editar Paquete";
   // Crear el elemento del modal
@@ -883,33 +885,35 @@ function editarPaquete() {
         </div>
         <div class="modal-body">
           <p>
-          
           <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-cursor-text"></i></span>
-            <input type="text" class="form-control" placeholder="Nombre del paquete" aria-label="nombre paquete" id="nombre_paquete" aria-describedby="basic-addon1">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-speedometer2"></i></span>
-            <input type="number" class="form-control" placeholder="Velocidad" aria-label="velocidad" id="velocidad_paquete" aria-describedby="basic-addon1">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-cash-coin"></i></span>
-            <input type="text" class="form-control" placeholder="Precio mensual" aria-label="precio" id="precio_paquete" aria-describedby="basic-addon1">
-            <span class="input-group-text" id="basic-addon1">.00</span>
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-toggles"></i></span>
-            <select class="form-select" aria-label="Default select example" id="estatus_paquete">
-                <option value="" selected>Estatus...</option>
-                <option value="1">Activo</option>
-                <option value="0">Inactivo</option>
-            </select>
-          </div>
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-calendar2-week"></i></span>
+              <input type="datetime-local" class="form-control" placeholder="" aria-label="Fecha de inicio" id="fecha_inicio" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-calendar2-week"></i></span>
+              <input type="datetime-local" class="form-control" placeholder="" aria-label="Fecha de fin" id="fecha_fin" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-cursor-text"></i></span>
+              <input type="text" class="form-control" placeholder="Nombre de la promoción" aria-label="nombre promocion" id="nombre_promocion" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-card-checklist"></i></span>
+              <select class="form-select" aria-label="Default select example" id="tipo_promo">
+                  <option value="" selected>Promoción en...</option>
+                  <option value="Mensualidad">Mensualidad</option>
+                  <option value="Instalación">Instalación</option>
+              </select>
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1"><i class="bi bi-piggy-bank"></i></span>
+              <input type="number" class="form-control" placeholder="Descuento" aria-label="descuento" id="descuento_promo" aria-describedby="basic-addon1">
+            </div>
           </p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          <button type="button" class="btn btn-primary" onclick="guardarPaquete()">Guardar</button>
+          <button type="button" class="btn btn-primary" onclick="guardarPromo()">Guardar</button>
         </div>
       </div>
     </div>
@@ -928,8 +932,8 @@ function editarPaquete() {
   });
 }
 
-function gestionPaquetes() {
-  let titulo = "Gestión de Paquetes";
+function gestionPromos() {
+  let titulo = "Gestión de Promociones";
   // Crear el elemento del modal
   const modal = document.createElement('div');
   modal.classList.add('modal', 'fade');
@@ -953,10 +957,10 @@ function gestionPaquetes() {
                 <div class="col-md-6">
                     <div class="input-group mb-3">
                         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                            <input type="radio" class="btn-check" value="1" name="btnradio" id="btnradio_paqA">
+                            <input type="radio" class="btn-check" value="1" name="btnradio" id="btnradio_promoA">
                             <label class="btn btn-outline-success" for="btnradio1"><i class="bi bi-check-lg"></i> Activo</label>
                         
-                            <input type="radio" class="btn-check" value="0" name="btnradio" id="btnradio_paqI">
+                            <input type="radio" class="btn-check" value="0" name="btnradio" id="btnradio_promoI">
                             <label class="btn btn-outline-danger" for="btnradio2"><i class="bi bi-x-lg"></i> Inactivo</label>
                         </div>
                     </div>
@@ -966,14 +970,16 @@ function gestionPaquetes() {
                       <thead>
                           <tr>
                               <th scope="col">Id</th>
-                              <th scope="col">Nombre Paquete</th>
-                              <th scope="col">Velocidad</th>
-                              <th scope="col">Mensualidad</th>
+                              <th scope="col">Nombre Promoción</th>
+                              <th scope="col">Tipo</th>
+                              <th scope="col">Descuento</th>
+                              <th scope="col">Inicio</th>
+                              <th scope="col">Final</th>
                               <th scope="col">Estatus</th>
                               <th scope="col" class="text-end"><i class="bi bi-people"></i></th>
                           </tr>
                       </thead>
-                      <tbody id="tablaPaquetes">
+                      <tbody id="tablaPromos">
                           
                       </tbody>
                   </table>
@@ -999,3 +1005,5 @@ function gestionPaquetes() {
     modal.remove();
   });
 }
+
+// Termina gestión de promociones
