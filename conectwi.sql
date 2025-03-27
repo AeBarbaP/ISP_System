813255d36df53c2177b746a4c570ef90c4836697
AnnyBarba <aebarbap@gmail.com>
2025-03-27 10:47:11 -0600

    base de datos actualizada 27 marzo

--- conectwi.sql
+++ conectwi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2025 at 04:15 PM
+-- Generation Time: Mar 27, 2025 at 05:45 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
