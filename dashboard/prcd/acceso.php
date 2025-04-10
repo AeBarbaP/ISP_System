3298a5cfa8ed0a8bcc05d270ebf0678e0aeea544
jesusrlv <jesusrlvrojo@gmail.com>
2025-04-10 01:15:45 -0600

    acceso ready

---
+++ dashboard/prcd/acceso.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+require('conn.php');
+
+$usr = $_POST['usr'];
+$pwd = $_POST['pwd'];
+
+$query = "SELECT * FROM users WHERE username = '$usr' AND pwd = '$pwd'";
+$resultado = $conn->query($query);
+$filas = $resultado->num_rows;
+
+if($filas == 1){ 
+    $_SESSION['sess'] = 1;
+    echo json_encode(array(
+        'success'=>1
+    ));
+}
+else{
+    $_SESSION['sess'] = "";
+    echo json_encode(array(
+        'success'=>0
+    ));
+}
