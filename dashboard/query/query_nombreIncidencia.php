f740a46b9ae9b6f77807950ad9c6fd3fe31da7dd
jesusrlv <jesusrlvrojo@gmail.com>
2025-05-26 12:45:27 -0600

    avanzando en incidencia tecnica

--- dashboard/query/query_nombreIncidencia.php
+++ dashboard/query/query_nombreIncidencia.php
@@ -7,9 +7,17 @@ $sql = "SELECT * FROM clientes WHERE folio = '$usuario'";
 $resultado = $conn->query($sql);
 $row = $resultado->fetch_assoc();
 
+$sqlIncidecia = "SELECT * FROM incidencias WHERE folio_cliente = '$usuario'";
+$resultadoIncidencia = $conn->query($sqlIncidecia);
+$rowIncidencia = $resultadoIncidencia->fetch_assoc();
+$incidecia = $rowIncidencia['descripcion'];
+$estatus = $rowIncidencia['estatus'];
+
 echo json_encode(array(
     'success'=>1,
-    'usuario'=>$row['nombre']
+    'usuario'=>$row['nombre'],
+    'incidencia'=>$incidecia,
+    'estatus'=>$estatus
 ));
 
 
