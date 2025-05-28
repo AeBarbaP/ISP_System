44b5b0884725bf2916163ed9b6dbc958ed3f413e
AnnyBarba <aebarbap@gmail.com>
2025-05-28 13:27:23 -0600

    acceso.php editado para revisar caja

--- dashboard/prcd/acceso.php
+++ dashboard/prcd/acceso.php
@@ -13,9 +13,12 @@ $filas = $resultado->num_rows;
 if($filas == 1){ 
     $_SESSION['sess'] = 1;
     $row = $resultado->fetch_assoc();
+
+    $id = $row['id'];
     
     $_SESSION['username'] = $row['username'];
     $_SESSION['nombre'] = $row['nombre'];
+    $_SESSION['estatusCorte'] = $revisarCorte($id);
 
     echo json_encode(array(
         'success'=>1
@@ -26,4 +29,23 @@ else{
     echo json_encode(array(
         'success'=>0
     ));
+}
+
+function revisarCorte($id){
+
+    $sqlCorte = "SELECT * FROM cortes WHERE usr ='$id'";
+    $resultadoCorte = $conn->query($sqlCorte);
+    $filas = $resultadoCorte->num_rows;
+
+    if($filas == 1){
+        $rowCorte = $resultadoCorte->fetch_assoc();
+        $corte = $rowCorte['estatus']; //esta variable confirma si está cortado el servicio o no
+        return $corte;
+    }
+    else{
+        $corte = 0;
+        return $corte;
+
+    }
+
 }
