871fe7f82258686e0da46588e5447f6f49f6b878
jesusrlv <jesusrlvrojo@gmail.com>
2025-07-02 13:06:20 -0600

    datos null en orden de compra

--- dashboard/query/query_infoClientesCorte.php
+++ dashboard/query/query_infoClientesCorte.php
@@ -13,11 +13,25 @@
     $resultado1 = $conn->query($sql1);
     $row1 = $resultado1->fetch_assoc();
 
-    $comunidad = $row1['comunidad'];
+    if ($row1 && (is_null($row1['comunidad']) || $row1['comunidad'] == "")) {
+    $comunidad = "Sin datos";
+    } else if ($row1) {
+        $comunidad = $row1['comunidad'];
+    } else {
+        $comunidad = "Sin datos";
+    }
+
+    if ($row1 && (is_null($row['domicilio']) || $row['domicilio'] == "")) {
+    $domicilio = "Sin datos";
+    } else if ($row1) {
+        $domicilio = $row['domicilio'];
+    } else {
+        $domicilio = "Sin datos";
+    }
 
     $clientes = array(
         'nombre' => $row['nombre'],
-        'domicilio' => $row['domicilio'],
+        'domicilio' => $domicilio,
         'comunidad' => $comunidad,
         'success'=> 1
     );
