bd163cc51b2e4f89073c101e2b871719b09c1c89
unknown <aebarbap@gmail.com>
2025-07-18 13:15:03 -0600

    conn modificado

--- dashboard/prcd/conn.php
+++ dashboard/prcd/conn.php
@@ -1,9 +1,13 @@
 <?php
-
     $servername="localhost";
-    $database="conectwi"; //solo se quitó para conexión remota
-    $username="root";
-    $password="";
+    $database="conectwi_conectwi"; //solo se quit贸 para conexi贸n remota
+    $username="conectwi_admin";
+    $password="Conect#12345";
+
+    //$servername="localhost";
+    //$database="conectwi"; //solo se quitó para conexión remota
+    //$username="root";
+    //$password="";
 
     $conn= new mysqli ($servername,$username,$password,$database);     
     $conn->set_charset("utf8");
