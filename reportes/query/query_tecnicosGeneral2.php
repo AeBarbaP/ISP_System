3c8dc7ed1aa3ba6eaefa58af99f1afceb7020b81
jesusrlv <jesusrlvrojo@gmail.com>
2025-07-19 14:39:31 -0600

    bugs de reportes

--- reportes/query/query_tecnicosGeneral2.php
+++ reportes/query/query_tecnicosGeneral2.php
@@ -32,12 +32,12 @@
 
         if($rowFallas['estatus'] == 1){
         echo'
-        <td><a href="#" onclick="cambiaEstatusTecnicoRep('.$id.')"><i class="bi bi-check-circle text-success"></i></a></td>
+        <td><a href="#" onclick="cambiaEstatusTecnicoRep('.$id.')">Activo <i class="bi bi-check-circle text-success"></i></a></td>
         ';
         }
         else{
             echo'
-            <td><a href="#" onclick="cambiaEstatusTecnicoRep('.$id.')"><i class="bi bi-x-circle-fill text-danger"></i></a></td>
+            <td><a href="#" onclick="cambiaEstatusTecnicoRep('.$id.')">Inactivo<i class="bi bi-x-circle-fill text-danger"></i></a></td>
             ';
         }
 
@@ -68,11 +68,7 @@
         }
 
         
-    echo'
-        <td><a href="#" data-bs-toggle="modal" data-bs-target="#reporteIndividualTecnico"  onclick="consultaTecnicosRep('.$id.')"><i class="bi bi-pen"></i></a></td> 
-        <!--Esta función no existe aún, es para llenar los campos del modal que genera reporte global o detallado de actividades de usuarios, hay que incluir login y logout además de los logs de usr-->
-    </tr>
-    ';
+    
     }
 
 ?>
